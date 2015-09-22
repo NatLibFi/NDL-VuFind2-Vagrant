3146e2b8a8cb8817453a72142d52954c6e3462e3
tomikkon <tomi.mikkonen@helsinki.fi>
2015-09-22 11:33:47 +0300

    Added conditioning to creation of database, user, files and links.

--- vufind2_bootstrap.sh
+++ vufind2_bootstrap.sh
@@ -23,14 +23,13 @@ sudo apt-get -y install mysql-server
 
 # create database and user & modify database
 MYSQL=`which mysql`
-Q1="CREATE DATABASE $DATABASE;"
-Q2="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$USER_PW';"
-Q3="GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USER'@'localhost';"
-Q4="FLUSH PRIVILEGES;"
-Q5="USE $DATABASE;"
-Q6="SOURCE $INSTALL_PATH/module/VuFind/sql/mysql.sql;"
-Q7="SOURCE $INSTALL_PATH/module/Finna/sql/mysql.sql;"
-SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}"
+Q1="CREATE DATABASE IF NOT EXISTS $DATABASE;"
+Q2="GRANT ALL ON $DATABASE.* TO '$USER'@'localhost' IDENTIFIED BY '$USER_PW';"
+Q3="FLUSH PRIVILEGES;"
+Q4="USE $DATABASE;"
+Q5="SOURCE $INSTALL_PATH/module/VuFind/sql/mysql.sql;"
+Q6="SOURCE $INSTALL_PATH/module/Finna/sql/mysql.sql;"
+SQL="${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
 $MYSQL -uroot -p$PASSWORD -e "$SQL"
 
 # install php 5.5
@@ -44,7 +43,9 @@ sudo a2enmod rewrite
 
 # link VuFind to Apache
 sudo cp /vagrant/httpd-vufind.conf /etc/apache2/conf-available/httpd-vufind.conf
-sudo ln -s /etc/apache2/conf-available/httpd-vufind.conf /etc/apache2/conf-enabled/vufind2.conf
+if [ ! -h /etc/apache2/conf-enabled/vufind2.conf ]; then
+  sudo ln -s /etc/apache2/conf-available/httpd-vufind.conf /etc/apache2/conf-enabled/vufind2.conf
+fi
 
 # restart apache
 service apache2 restart
@@ -52,10 +53,10 @@ service apache2 restart
 # copy sample configs to ini files
 cd $INSTALL_PATH/local/config/finna
 for x in *.ini.sample; do 
- t=${x%.ini.sample}.ini
- if [ ! -f $t ]; then
+  t=${x%.ini.sample}.ini
+  if [ ! -f $t ]; then
     cp $x $t
- fi
+  fi
 done
 
 cd $INSTALL_PATH/local/config/vufind
@@ -68,7 +69,7 @@ done
 cp searchspecs.yaml.sample searchspecs.yaml
 
 # copy local dir inside virtual machine
-sudo mkdir /usr/local/vufind2_local
+sudo mkdir -p /usr/local/vufind2_local
 sudo cp -rf $INSTALL_PATH/local/* /usr/local/vufind2_local/
 sudo chown -R vagrant:vagrant /usr/local/vufind2_local
 sudo chown -R www-data:www-data /usr/local/vufind2_local/cache
