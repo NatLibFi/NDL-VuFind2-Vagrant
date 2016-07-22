4d7e3a4be12436d37173ca2cf4812f7637ff1ce3
tomikkon <tomi.mikkonen@helsinki.fi>
2016-07-22 12:01:17 +0300

    no message

--- bootstrap_vufind2_centos.sh
+++ bootstrap_vufind2_centos.sh
@@ -19,7 +19,7 @@ USER_PW='vufind'
 EXTERNAL_SOLR_URL=''
 
 # Oracle PHP OCI Instant Client (Voyager)
-INSTALL_ORACLE_CLIENT=false         # Make sure you have the installer RPM files
+INSTALL_ORACLE_CLIENT=true         # Make sure you have the installer RPM files
 ORACLE_PATH='/vagrant/oracle'      # downloaded here from Oracle Downloads.
 CONFIG_PATH='/vagrant/oracle'      # Voyager config files.
 # version info
@@ -28,13 +28,13 @@ OCI_VERSION='12.1'
 OCI_CONFIG_URL='http://pastebin.com/raw/20T49aHg'  # 20T49aHg <= v12.1
 
 # Solr
-INSTALL_SOLR=false                  # If true you will also need RecordManager!
+INSTALL_SOLR=true                  # If true you will also need RecordManager!
 SOLR_PATH='/data/solr'             # Separately installing one without the other
 JAVA_HEAP_MIN='256m'               # is only useful for debugging the install
 JAVA_HEAP_MAX='512m'               # process if errors arise.
 
 # RecordManager
-INSTALL_RM=false
+INSTALL_RM=true
 RM_PATH='/usr/local/RecordManager'
 SAMPLE_DATA='/vagrant/data/sample.xml'  # use MARC
 
