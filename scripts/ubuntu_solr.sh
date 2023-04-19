eabfe36dfec6ceb66b9909270527fa8ba7085c34
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2023-04-19 17:15:39 +0300

    Proper Finna-solr installation using Solr 9 (ubuntu)

--- scripts/ubuntu_solr.sh
+++ scripts/ubuntu_solr.sh
@@ -11,6 +11,11 @@ echo "Installing Solr..."
 echo "=================="
 # install Java OpenJDK
 sudo apt-get install -y openjdk-$JAVA_VERSION-jdk-headless
+# set memory & open files limit
+if [ "$JAVA_SET_SHMMAX_OPENFILES" = true ]; then
+    echo $JAVA_SHMMAX | sudo tee /proc/sys/kernel/shmmax
+    echo -e "soft nofile $JAVA_OPENFILES_LIMIT\nhard nofile $JAVA_OPENFILES_LIMIT" | sudo tee -a /etc/sysctl.conf
+fi
 
 # install Solr
 sudo mkdir -p $SOLR_PATH
@@ -22,21 +27,20 @@ fi
 sudo su -c "useradd solr -m"
 #sudo su -c 'echo solr:rlos | chpasswd'
 
-# set Solr version
-echo $SOLR_VERSION | sudo tee $SOLR_PATH/required_solr_version
-
 cd $SOLR_PATH
 sudo ./install_solr.sh
 sudo cp $SOLR_PATH/vufind/solr.in.finna.sh.sample $SOLR_PATH/vufind/solr.in.finna.sh
 sudo cp $SOLR_PATH/vufind/biblio/core.properties.sample-non-solrcloud $SOLR_PATH/vufind/biblio/core.properties
 sudo chown -R solr $SOLR_PATH
 
-# set java heap min/max
+# set Java heap min/max
 sudo sed -i 's/SOLR_JAVA_MEM=/#SOLR_JAVA_MEM=/' $SOLR_PATH/vufind/solr.in.finna.sh
 sudo sed -i '/#SOLR_JAVA_MEM/a SOLR_JAVA_MEM="-Xms'"$JAVA_HEAP_MIN"' -Xmx'"$JAVA_HEAP_MAX"'"' $SOLR_PATH/vufind/solr.in.finna.sh
-# disable solrcloud
+# disable Zookeeper & SolrCloud
 sudo sed -i 's/ZK_/#ZK_=/' $SOLR_PATH/vufind/solr.in.finna.sh
-sudo sed -i 's/SOLR_MODE/#SOLR_MODE=/' $SOLR_PATH/vufind/solr.in.finna.sh
+sudo sed -i 's/SOLR_MODE/#SOLR_MODE/' $SOLR_PATH/vufind/solr.in.finna.sh
+# set allowed IPs
+sudo sed -i -e '$ a SOLR_JETTY_HOST=\"'"$SOLR_JETTY_HOST"'\"' $SOLR_PATH/vufind/solr.in.finna.sh
 
 #set as service
 sudo cp $SOLR_PATH/vufind/solr.service /etc/systemd/system
