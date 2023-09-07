743b21c2eff2a3ff3d36c9e258ec0b5a0d6c13ff
tmikkonen <1330453+tmikkonen@users.noreply.github.com>
2023-09-07 17:39:08 +0300

    Update nodeJS install script/method (ubuntu)

--- scripts/ubuntu_vufind2.sh
+++ scripts/ubuntu_vufind2.sh
@@ -103,11 +103,17 @@ sudo chown www-data:www-data /var/log/vufind2.log
 
 # install node.js & less 2.7.1 + less-plugin-clean-css
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE="DontWarn"
-curl -sL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
 sudo apt-get install -y nodejs
 sudo npm install -g less@$LESS_VERSION
 sudo npm install -g less-plugin-clean-css
 sudo npm install -g eslint@$ESLINT_VERSION
+#check npm vulnerabilities
+npm init -y
+npm i --package-lock-only
+npm audit
 # do not run this with sudo
 tee -a /usr/local/bin/eslint-finna >/dev/null <<EOF
 cd /vufind2
